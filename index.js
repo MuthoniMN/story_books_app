11a932d34e899654eee97463f190e3d476b9be86
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-11-07 18:42:56 +0300

    creating db.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const dotenv = require('dotenv')
 
 const app = express()
+// load the environment variables
 dotenv.config({ path: './config/config.env' })
+const PORT = process.env.PORT || 5000
 
-app.listen(process.env.PORT, () => console.log("Server is running"))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode.`))
