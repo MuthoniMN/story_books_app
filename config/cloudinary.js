804157a6cf911feab577a215583c1a572c07c9b7
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-11-15 20:49:40 +0300

    using cards to display stories

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "./Images")
+        cb(null, "./public/Images")
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}${path.extname(file.originalname)}`)
--- routes/stories.js
+++ routes/stories.js
@@ -13,10 +13,11 @@ router.get('/add', ensureAuth, (req, res) => {
 // @desc    Add a story
 // @route  POST /stories/add
 router.post('/add', ensureAuth, upload.single("featuredImage"), async (req, res) => {
+    console.log(req.body)
     try {
         req.body.user = req.user.id
-        req.body.published = req.body.published === "Publish" ? true : false
-        req.body.featuredImage = req.file.path
+        req.body.published = req.body.published === "publish" ? true : false
+        req.body.featuredImage = req.file.path.substring(req.file.path.indexOf('/') + 1)
 
         await Story.create(req.body)
         res.redirect('/dashboard')
