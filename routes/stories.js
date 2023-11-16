00f59bb6743be303131011f968fa9d26e038200a
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-11-16 16:10:01 +0300

    slight modification of story route

--- routes/stories.js
+++ routes/stories.js
@@ -47,11 +47,20 @@ router.get('/:id', ensureAuth, async (req, res) => {
     try {
         const story = await Story.findById(req.params.id).populate('user').limit(3).lean()
         const stories = await Story.find({ user: req.user.id, _id: { $ne: req.params.id } }).lean()
-        res.render('stories/story', {
-            story,
-            stories,
-            user: req.user
-        })
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            res.render('stories/story', {
+                story,
+                stories,
+                user: req.user
+            })
+        }
     } catch (err) {
         console.error(err)
         res.render('errors/500')
